2068f6dff3d85d040cd084fec12ae837b52b03b6
Tapas kumar palai <140144696+Tapas-kp@users.noreply.github.com>
2024-04-03 00:21:43 +0530

    Add files via upload
    fun-1

---
+++ function1.js
@@ -0,0 +1,31 @@
+ function primenot(num) {
+    let count=0
+    for(let i=1;i<=num;i++){
+        if(num%i==0){
+            count+=1
+        }
+       
+    }
+    if(count==2){
+        return true
+    }
+    else{
+        return false
+    }
+    
+}
+function check(limit){
+    for(i=2;i<=limit;i++){
+        let a=primenot(i)
+        
+        if(a==true){
+            console.log(i,"prime")
+        }
+        else{
+            console.log(i,"Not")
+        }
+    }
+
+
+}
+check(100)
